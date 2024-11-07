06a9a46ae2da038e01d6ff755796e0ebcf84fec3
Oleksii-Danylov <oleksiidanylov1988@gmail.com>
2024-11-08 00:21:30 +0200

    implement-search-methods

--- personal_assistant/notes.py
+++ personal_assistant/notes.py
@@ -1,8 +1,4 @@
 class Note:
-    def __init__(self, title, content):
-        self.title = title
-        self.content = content
-    
     def __init__(self, title, content, tags=None):
         self.title = title
         self.content = content
@@ -12,5 +8,19 @@ class NoteBook:
     def __init__(self):
         self.notes = {}
     
-    def add_note(self, note):
+    def add_note(self, note): # Функція додавання запису в примітки
         self.notes[note.title] = note
+    
+    def search_notes(self, query): # Функція пошуку
+        results = []
+        for note in self.notes.values():
+            if query.lower() in note.content.lower() or query.lower() in note.title.lower():
+                results.append(note)
+        return results
+
+    def search_by_tags(self, tags): # Функція пошуку за тегом
+        results = []
+        for note in self.notes.values():
+            if set(tags).issubset(set(note.tags)):
+                results.append(note)
+        return results
