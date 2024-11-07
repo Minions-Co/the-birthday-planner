8bdf74baea89b908637d8809f892bb32337ad3bc
Oleksii-Danylov <oleksiidanylov1988@gmail.com>
2024-11-07 23:28:37 +0200

    add-tags-to-note-class

--- personal_assistant/notes.py
+++ personal_assistant/notes.py
@@ -2,3 +2,8 @@ class Note:
     def __init__(self, title, content):
         self.title = title
         self.content = content
+    
+    def __init__(self, title, content, tags=None):
+        self.title = title
+        self.content = content
+        self.tags = tags if tags else []
