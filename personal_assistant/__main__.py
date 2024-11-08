fd1a538ed61a5620c467aaea488c20e8846dc953
Dimm <drimmworks@protonmail.ch>
2024-11-08 17:41:15 +0200

    create dummy main function, add package version

---
+++ personal_assistant/__main__.py
@@ -0,0 +1,11 @@
+import sys
+#from personal_assistant.contacts import
+#from personal_assistant.notes import
+#from personal_assistant.utils import
+#from personal_assistant.exceptions import
+
+def main():
+    print("Вітаємо у Personal Assistant!")
+
+if __name__ == '__main__':
+    main()
