f481353fdaadb9ff105a8b6f9b5f7315e5dae52e
wital1y <guz.vitaliy.oleksandrovich@gmail.com>
2024-11-08 15:22:06 +0100

    Виправлено скрипт для роботи з JSON файлами у класі Storage

--- personal_assistant/storage.py
+++ personal_assistant/storage.py
@@ -3,14 +3,16 @@ import os
 
 class Storage:
     def __init__(self, filename):
-        self.filepath = os.path.join(os.path.expanduser('~'), filename)
+        self.filename = filename
+        self.filepath = os.path.join(os.path.expanduser('~'), 'personal_assistant_data', self.filename)
+        os.makedirs(os.path.dirname(self.filepath), exist_ok=True)
 
     def load_data(self):
         if not os.path.exists(self.filepath):
             return {}
-        with open(self.filepath, 'r') as f:
+        with open(self.filepath, 'r', encoding='utf-8') as f:
             return json.load(f)
 
     def save_data(self, data):
-        with open(self.filepath, 'w') as f:
-            json.dump(data, f)
+        with open(self.filepath, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
