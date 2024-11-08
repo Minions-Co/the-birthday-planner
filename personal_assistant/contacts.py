d4390a94e1b6230cb0d86e7468c4f1558946a5a2
astechad <astech.ad@gmail.com>
2024-11-08 18:18:13 +0200

    add phone validation

--- personal_assistant/contacts.py
+++ personal_assistant/contacts.py
@@ -1,5 +1,19 @@
+import re
+
+
+class InvalidPhoneError(Exception):
+    pass
+
+
 class Contact:
-    def __init__(self, name, phone, email):
+    class Contact:
+
+    def __init__(self, name, phone='', email=''):
         self.name = name
         self.phone = phone
         self.email = email
+
+    def validate_phone(self):
+        pattern = r'^\+?\d{9,15}$'
+        if not re.match(pattern, self.phone):
+            raise InvalidPhoneError("Invalid phone number.")
